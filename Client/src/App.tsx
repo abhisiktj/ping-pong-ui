aaa27056024c3032a40b84f4026594212972f6c4
unknown <abhisiktj@gmail.com>
2024-11-11 20:50:47 +0530

    added mui

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDom from "react-dom/client";
 import About from "./Pages/About";
 import NotFound from "./Pages/NotFound";
+
 import {
   createBrowserRouter,
   createRoutesFromElements,
--- Client/src/Pages/About.tsx
+++ Client/src/Pages/About.tsx
@@ -1,9 +1,17 @@
-import React from "react"
+import React from "react";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { Typography } from "@mui/material";
 
-
-const About:React.FC=()=>{
-    return(
-        <h1>Ping-Pong:Pings Your websites to notify you pongs!</h1>
-    )
-}
+const About: React.FC = () => {
+  return (
+    <>
+      <Typography variant="h5">
+        Pings Your websites to notify you pongs!
+      </Typography>
+    </>
+  );
+};
 export default About;
